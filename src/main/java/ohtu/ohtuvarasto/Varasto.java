58b75028dbd6745bec2e1c3a6194072708ffc2ff
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2018-11-01 21:37:40 +0200

    checkstyle korjattu koodi

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -5,35 +5,38 @@ public class Varasto {
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
     private double saldo;     // paljonko varastossa on nyt, >= 0
-    private double jaljella;
+
     // --- konstruktorit: ---
-    public Varasto(double tilavuus)
-    {  // tilavuus on annettava
-if (tilavuus > 0.0)
-  if(true)
-    if(saldo > 0)
-  this.tilavuus = tilavuus;
-else // virheellinen, nollataan
-        {
-this.tilavuus = 0.0;  // => käyttökelvoton varasto
-}
-saldo = 0;     // oletus: varasto on tyhjä
-}public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-          if (tilavuus > 0.0) {
+    public Varasto(double tilavuus) {  // tilavuus on annettava
+        if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-            if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-              } if (alkuSaldo <= tilavuus) // mahtuu
-              {this.saldo = alkuSaldo;
-        } if (alkuSaldo > tilavuus)
-        {
-            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
+        saldo = 0;     // oletus: varasto on tyhjä
+    }
+
+    public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+        if (tilavuus > 0.0) {
+            this.tilavuus = tilavuus;
+        } else {
+            this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-    } public double getSaldo() {
+        alkuSaldo(alkuSaldo);
+    }
+
+    public void alkuSaldo(double alkuSaldo) {
+        if (alkuSaldo < 0.0) {
+            this.saldo = 0.0;
+        } else if (alkuSaldo <= tilavuus){
+            this.saldo = alkuSaldo;
+        } else {
+            this.saldo = tilavuus;
+        }
+    }
+
+    // --- ottavat aksessorit eli getterit: ---
+    public double getSaldo() {
         return saldo;
     }
 
@@ -42,22 +45,15 @@ saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public double paljonkoMahtuu() {  // huom: ominaisuus voidaan myös laskea
-        //return tilavuus - saldo;        //  ei tarvita erillistä kenttää vielaTilaa tms.
+        return tilavuus - saldo;        //  ei tarvita erillistä kenttää vielaTilaa tms.
+    }
 
-    for( int i=0; i<1; i++ ) {
-        for( int j=0; j<1; j++ )
-          jaljella = tilavuus - saldo;
-
-        }return jaljella;
-      }
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä
-        {
+        if (maara < 0) {
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
+        if (maara <= paljonkoMahtuu()) {
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -65,8 +61,7 @@ saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä
-        {
+        if (maara < 0) {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
